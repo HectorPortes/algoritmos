7172136b3bc56d3a17c0a8bb046071c91167ade2
HectorPortes <hectorportes80@gmail.com>
2025-08-27 19:45:18 -0400

    a

--- a1/aula1.js
+++ a1/aula1.js
@@ -1,3 +1,7 @@
 let primeiroCodigo = "Hello World!"
 console.log(primeiroCodigo)
 
+// para executar um arquivo caso o pc não tenha coderuner instalado
+// aperte ctrl + " ou la em cima nos ... -> terminal -> novo terminal
+// exemplo como vai estar: C:\Users\aluno\Documents\sl>
+// para executar o arquivo que você quer basta escrever: node nome_arquivo
