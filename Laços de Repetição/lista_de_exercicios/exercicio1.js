d4cd3a701b0b64d905ce3815877004e9d1a632ab
Héctor Portes <141651226+HectorPortes@users.noreply.github.com>
2025-10-02 02:32:05 -0300

    Add files via upload

---
+++ Laços de Repetição/lista_de_exercicios/exercicio1.js
@@ -0,0 +1,12 @@
+// Exercício 1: Soma dos Primeiros Inteiros
+// (Complexidade: Baixa) - Dado um inteiro positivo n, calcule a soma dos n primeiros inteiros positivos.
+
+// Exemplo de Interação:
+
+// Digite o valor de n: 10
+// ---
+// Saída no Console:
+// A soma dos 10 primeiros inteiros positivos é 55.
+const prompt = require('prompt-sync')()
+
+// Ainda vou fazer os exercicios
